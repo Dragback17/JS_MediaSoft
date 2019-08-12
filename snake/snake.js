01644f8759c6f9c64276e36c41a651a37dea6ec0
Dragback17 <dragback@mail.ru>
2019-08-12 21:42:50 +0300

    move

--- snake/snake.js
+++ snake/snake.js
@@ -53,6 +53,28 @@ function createCoor() {
 
 }
 
+function createEat() {
+
+	let eatCoor = createCoor();
+
+	let eat = document.querySelector('[posX = "' + eatCoor[0] + '"][posY = "' + eatCoor[1] + '"]');		
+
+	while (eat.classList.contains('snakeBody')) {
+
+		let eatCoor = createCoor();
+
+		let eat = document.querySelector('[posX = "' + eatCoor[0] + '"][posY = "' + eatCoor[1] + '"]');
+
+	}
+
+	eat.classList.add('eat');
+
+	return eat;
+
+}
+
+let eat1 = createEat();
+
 function createSnake() {
 	
 	let snakeCoor = createCoor();
@@ -67,23 +89,105 @@ document.querySelector('[posX = "' + (snakeCoor[0] - 2) + '"][posY = "' + snakeC
 
 	}
 
+	let direction = "right";
+
+window.addEventListener('keydown', function (e) {
+
+
+		if (e.keyCode == 37 && direction != 'right'){
+
+			direction = 'left';
+
+		}
+		else if (e.keyCode == 38 && direction != 'down'){
+			direction = 'up';
+
+		}
+		else if (e.keyCode == 39 && direction != 'left'){
+
+			direction = 'right';
+
+		}
+		else if (e.keyCode == 40 && direction != 'up'){
+
+			direction = 'down';
+
+		}
+	});
+
 	function snakeMove() {
-	
+
 		let snakeCoordinates = [snakeBody[0].getAttribute('posX'), snakeBody[0].getAttribute('posY')];
 
 		snakeBody[snakeBody.length-1].classList.remove('snake');
 
 		snakeBody.pop();
 
-		if (snakeCoordinates[0] < 25) {
+		if (direction == 'right') {
+
+			if (snakeCoordinates[0] < 25) {
 
 			snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] + 1) + 
 				'"][posY = "' + snakeCoordinates[1] + '"]'));
 
-		} else {
+			} else {
 
-			snakeBody.unshift(document.querySelector('[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'));
+				snakeBody.unshift(document.querySelector('[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'));
+			}
+
+		}   else if (direction == 'left') {
+
+			if (snakeCoordinates[0] > 1) {
+
+			snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] - 1) + 
+				'"][posY = "' + snakeCoordinates[1] + '"]'));
+
+			} else {
+
+				snakeBody.unshift(document.querySelector('[posX = "25"][posY = "' + snakeCoordinates[1] + '"]'));
+			}
+
+		}   else if (direction == 'up') {
+
+			if (snakeCoordinates[1] < 25) {
+
+			snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + 
+				(+snakeCoordinates[1] + 1) + '"]'));
+
+			} else {
+
+				snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "1"]'));
+			}
+
+		}   else if (direction == 'down') {
+
+			if (snakeCoordinates[1] > 1) {
+
+			snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + 
+				(+snakeCoordinates[1] - 1) + '"]'));
+
+			} else {
+
+				snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0]  + '"][posY = "25"]'));
+			}
+
 		}
+
+		if (snakeBody[0].getAttribute('posX') == eat1.getAttribute('posX') && 
+			snakeBody[0].getAttribute('posY') == eat1.getAttribute('posY')) {
+
+			eat1.classList.remove('eat');
+
+			let x = snakeBody[snakeBody.length - 1].getAttribute('posX');
+
+			let y = snakeBody[snakeBody.length - 1].getAttribute('posY');
+
+			snakeBody.push(document.querySelector('[posX = "' + x + '"][posY = "' + y + '"]'));
+
+			eat1 = createEat();
+
+			}
+		
 
 		for (let i = 0; i < snakeBody.length; i++) {
 
@@ -99,25 +203,6 @@ document.querySelector('[posX = "' + (snakeCoor[0] - 2) + '"][posY = "' + snakeC
 	
 createSnake();
 
-function createEat() {
-
-	let eatCoor = createCoor();
-
-	let eat = document.querySelector('[posX = "' + eatCoor[0] + '"][posY = "' + eatCoor[1] + '"]');
-
-	while (eat.classList.contains('snakeBody')) {
-
-		let eatCoor = createCoor();
-
-		let eat = document.querySelector('[posX = "' + eatCoor[0] + '"][posY = "' + eatCoor[1] + '"]');
-
-	}
-
-	eat.classList.add('eat');
-	
-}
-
-createEat();
 
 
 
